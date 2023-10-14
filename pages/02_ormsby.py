90d65ba6b7deebf512ce5c4daafa390e8f0f7222
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-10-13 23:57:04 -0600

    Update 02_ormsby.py

--- pages/02_ormsby.py
+++ pages/02_ormsby.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-#from scipy.signal import hilbert, chirp
+from scipy.signal import hilbert
 import math
+pi = math.pi
 
 pi = math.pi
 
@@ -47,6 +48,9 @@ f2 = st.slider('Select frequency f2 (Hz)', value=10., min_value=1., max_value=24
 f3 = st.slider('Select frequency f3 (Hz)', value=60., min_value=1., max_value=240.)
 f4 = st.slider('Select frequency f4 (Hz)', value=70., min_value=1., max_value=240.)
 st.write(f1, " - ", f2, " - ", f3, " - ", f4)
+
+phi = st.slider('Phase rotation angle (deg)', value=0.0, min_value=0., max_value=360.)
+st.write("Phi = ", phi)
 
 t, y = ORMSBY(f1, f2, f3, f4, 0.512, 0.001)
 
