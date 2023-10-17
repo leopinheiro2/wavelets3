4f1f532bb40cf7211030c73a6d036eb17f98e7b6
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-10-16 23:14:28 -0600

    Update 02_ormsby.py

--- pages/02_ormsby.py
+++ pages/02_ormsby.py
@@ -28,32 +28,18 @@ st.latex(r'''
     - \frac{\pi f_2^2 sinc^2 (\pi f_2 t) - \pi f_1^2 sinc^2 (\pi f_1 t)}{f_2 - f_1}
     ''') 
 
-#st.latex(r'''
-#    Ormsby(t) = \frac{\pi^2f_4^2}{\pi f_4 - \pi f_3} sinc^2 (\pi f_4 t) 
-#    - \frac{\pi^2f_3^2}{\pi f_4 - \pi f_3} sinc^2 (\pi f_3 t)
-#    - \frac{\pi^2f_2^2}{\pi f_2 - \pi f_1} sinc^2 (\pi f_4 t)
-#    + \frac{\pi^2f_1^2}{\pi f_2 - \pi f_1} sinc^2 (\pi f_4 t)
-#    ''') 
-
-#f = st.slider('Select frequency from [1, 240] Hz', value=60., min_value=1., max_value=240.)
-#st.write("Frequency f3 = ", f)
-
-#f1 = 5
-#f2 = 10
-#f3 = 60
-#f4 = 70.
 col1, col2 = st.columns(2)
 with col1:
-    f1 = st.slider('Select frequency f1 (Hz)', value=5., min_value=1., max_value=240.)
-    f2 = st.slider('Select frequency f2 (Hz)', value=10., min_value=1., max_value=240.)
+    f1 = st.slider('Select frequency f1 (Hz)', value=5., min_value=1., max_value=240., step=1., format="%.1f")
+    f3 = st.slider('Select frequency f3 (Hz)', value=60., min_value=1., max_value=240., step=1., format="%.1f")
 
 with col2:
-    f3 = st.slider('Select frequency f3 (Hz)', value=60., min_value=1., max_value=240.)
-    f4 = st.slider('Select frequency f4 (Hz)', value=70., min_value=1., max_value=240.)
+    f2 = st.slider('Select frequency f2 (Hz)', value=10., min_value=1., max_value=240., step=1., format="%.1f")
+    f4 = st.slider('Select frequency f4 (Hz)', value=70., min_value=1., max_value=240., step=1., format="%.1f")
 st.write(f1, " - ", f2, " - ", f3, " - ", f4)
 
 phi = st.slider('Phase rotation angle (deg)', value=0.0, min_value=0., max_value=360.)
-st.write("Phi = ", phi)
+#st.write("Phi = ", phi)
 str1 = "ORMSBY " + str(int(f1 + 0.5)) + " - " + str(int(f2 + 0.5))  + " - " + str(int(f3 + 0.5)) + " - " + str(int(f4 + 0.5)) + " Hz, Phase " + str(int(phi+0.5)) + "°"
 st.subheader(str1)
 
