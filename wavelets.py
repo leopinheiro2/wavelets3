3818507f19b2617fa7189f44896ebd168b3d26d5
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-11-10 14:00:49 -0700

    Update wavelets.py

--- wavelets.py
+++ wavelets.py
@@ -7,7 +7,7 @@ url = "https://www.rmseismic.com"
 st.write("RM Seismic Software [rmseismic.com](%s)" % url)
 st.write('The purpose of this web app is to illustrate how basic seismic wavelets change with the change of parameters, and calculate synthetic seisimc.')
 st.write('You can also covolve any of these wavelets with simple reflectivity series and calculate a synthetic trace.')
-st.write('Waveler equations are from a paper by Harold Ryan: Ricker, Ormsby; Klander, Bntterworth - A Choice of wavelets, publised in CSEG Recorder, September 1994.')
+st.write('Wavelet equations used in this app are from a paper by Harold Ryan: Ricker, Ormsby; Klander, Bntterworth - A Choice of wavelets, publised in CSEG Recorder, September 1994.')
 st.write('A list of wavelets you can display and vary parameters:')
 st.write('**Ricker, Ormsby, Klauder** - use left side menu to select a wavelet and sliders to change parameters.')
 
